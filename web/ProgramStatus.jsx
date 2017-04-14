fae1b09fcd713e88cc5c70de41ce47fe4a3dd12a
Ben <mail@ben-ha.com>
2017-04-14 06:11:43 +0000

    Refactored status bar

---
+++ web/ProgramStatus.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {RegisterDataStoreCallback} from './DataStore.js';
+
+class ProgramStatus extends React.Component {
+
+    constructor(props)
+    {
+	super(props);
+    }
+
+    componentDidMount()
+    {
+	RegisterDataStoreCallback(this._OnDataStoreChanged.bind(this));
+    }
+
+    _OnDataStoreChanged(store)
+    {
+	this.setState({status : store.ProgramState});
+    }
+    
+    _GetStatusNode()
+    {
+	if (this.state == null || this.state.status == undefined)
+	    return;
+	
+	let alert_style = "";
+	let alert_msg = "";
+	
+	switch(this.state.status)
+	{
+	    case "NotStarted":
+	    alert_style = "alert-info";
+	    alert_msg = "Not started";
+	    break;
+	    case "Running":
+	    alert_style = "alert-success";
+	    alert_msg = "Running";
+	    break;
+	    case "Stopped":
+	    alert_style = "alert-warning";
+	    alert_msg = "Paused";
+	    break;
+	    case "Terminated":
+	    alert_style = "alert_info";
+	    alert_msg = "Terminated";
+	}
+
+	return (<div className={"alert " + alert_style} style={{padding: "1px"}}>{alert_msg}</div>);
+    }
+    
+    render() {
+      	return (
+		<div>
+	    {this._GetStatusNode()}
+		</div>
+	    );
+}
+}
+
+export default ProgramStatus;
--- web/StatusBar.jsx
+++ web/StatusBar.jsx
@@ -1,60 +1,14 @@
 import React from 'react';
-import {RegisterDataStoreCallback} from './DataStore.js';
+import ProgramStatus from './ProgramStatus.jsx';
 
 class StatusBar extends React.Component {
-
-    constructor(props)
-    {
-	super(props);
-    }
-
-    componentDidMount()
-    {
-	RegisterDataStoreCallback(this._OnDataStoreChanged.bind(this));
-    }
-
-    _OnDataStoreChanged(store)
-    {
-	this.setState({status : store.ProgramState});
-    }
-    
-    _GetStatusNode()
-    {
-	if (this.state == null || this.state.status == undefined)
-	    return;
-	
-	let alert_style = "";
-	let alert_msg = "";
-	
-	switch(this.state.status)
-	{
-	    case "NotStarted":
-	    alert_style = "alert-info";
-	    alert_msg = "Not started";
-	    break;
-	    case "Running":
-	    alert_style = "alert-success";
-	    alert_msg = "Running";
-	    break;
-	    case "Stopped":
-	    alert_style = "alert-warning";
-	    alert_msg = "Paused";
-	    break;
-	    case "Terminated":
-	    alert_style = "alert_info";
-	    alert_msg = "Terminated";
-	}
-
-	return (<div className={"alert " + alert_style} style={{padding: "1px"}}>{alert_msg}</div>);
-    }
-    
-    render() {
+      render() {
       	return (
-		<div className="row">
-		    <div className="col-sm-1">
-	    {this._GetStatusNode()}
-	    </div>
+	    <div className="row">
+		<div className="col-sm-1">
+		    <ProgramStatus />
 		</div>
+	    </div>
 	    );
 }
 }
