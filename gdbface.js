d1312a431a3c19e52167457561152206446a85aa
Ben <mail@ben-ha.com>
2017-04-15 21:29:00 +0000

    Added usage

--- gdbface.js
+++ gdbface.js
@@ -4,8 +4,26 @@ var engine = require("./engine.js");
 
 var params = {};
 
+function PrintUsage()
+{
+	console.log("Usage:\tgdbface <program> [args]");
+	console.log("\tgdbface -p <pid>");
+}
+
+if (process.argv[2] == "--help" || process.argv[2] == "-h")
+{
+	PrintUsage();
+	process.exit();		
+}
+
 if (process.argv[2] == "-p")
 {
+	if (process.argv.length != 4)
+	{
+		PrintUsage();
+		process.exit();
+	}
+
 	params["pid"] = parseInt(process.argv[3]);
 }
 else
